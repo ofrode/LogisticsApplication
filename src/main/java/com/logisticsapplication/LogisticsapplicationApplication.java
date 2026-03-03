21776e1ef0413b7070e1221f32f3f418e3d20bc0
ofrode <ofrode.nn@gmail.com>
2026-03-03 22:26:48 +0300

    CheckStyle

--- src/main/java/com/logisticsapplication/LogisticsapplicationApplication.java
+++ src/main/java/com/logisticsapplication/LogisticsapplicationApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LogisticsapplicationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LogisticsapplicationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LogisticsapplicationApplication.class, args);
+    }
 
 }
