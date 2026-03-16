0d37aade4b06ab364fcde82169a4f8c22d5f2548
ofrode <ofrode.nn@gmail.com>
2026-03-16 12:56:37 +0300

    Fix

--- src/main/java/com/logisticsapplication/LogisticsApplication.java
+++ src/main/java/com/logisticsapplication/LogisticsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LogisticsapplicationApplication {
+public class LogisticsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LogisticsapplicationApplication.class, args);
+        SpringApplication.run(LogisticsApplication.class, args);
     }
 
 }
